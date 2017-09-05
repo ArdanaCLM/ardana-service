bbaf6dc393fbe06e99db2767007869bbce2f6f46
Gary Smith <gary.smith@suse.com>
2017-09-05 12:37:53 -0700

    XXX-0000 Persist playbook events
    Save playbook events when they are received, and play them back along
    with the log when a client joins a playbook in progress
    
    Change-Id: I1516727b98cb2fcd544e78476c375877bf755f45

--- ardana_service/listener.py
+++ ardana_service/listener.py
@@ -1,29 +1,58 @@
+from flask import abort
 from flask import Blueprint
 from flask import jsonify
 from flask import request
+import json
 import logging
+import os
+import time
 
+from . import config
 from . import socketio
+from .playbooks import get_events_file
 
 LOG = logging.getLogger(__name__)
 
 bp = Blueprint('listener', __name__)
 
-connection_timeout = 86400  # 24 hours
-
-connections = {}
+LOGS_DIR = config.get_dir("log_dir")
 
 
 @bp.route("/api/v2/listener/playbook/<event>/<path:name>", methods=['POST'])
 def playbook_event(event, name):
     """Event listener for playbook events
 
-    Will propagate the start event to listeners that are registered
+    Propagate events to socketio listeners
     """
     opts = request.get_json() or {}
     if 'id' in opts:
         id = str(opts['id'])
-        socketio.emit("playbook-" + event, name, room=id)
-        return jsonify(id)
+
+        playbook_event = "playbook-" + event
+        socketio.emit(playbook_event, name, room=id)
+
+        # Record event to file so that it can be replayed back to any client
+        # that joins while the playbook is already underway
+        events_file = get_events_file(id)
+        try:
+            if os.path.exists(events_file):
+                with open(events_file) as f:
+                    events = json.load(f)
+            else:
+                events = []
+
+            events.append({'event': playbook_event,
+                           'playbook': name,
+                           'timestamp': int(time.time())})
+
+            with open(events_file, "w") as f:
+                json.dump(events, f)
+
+            return jsonify(id)
+
+        except IOError as e:
+            LOG.exception(e)
+            abort(500, "Unable to write event")
+
     else:
         return ('', 201)
