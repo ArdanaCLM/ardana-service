bbb7db1c765954829550578a8dd8b3bd833b6fa4
Jack Choy <jack.likes.to.code@gmail.com>
2017-12-05 15:43:35 -0800

    SCRD-0000 Fix username error
    This fixes the problem where attempts to get the username would
    result in a "No such file or directory" error.
    
    Change-Id: Ib6791a2f5eceac4b0b02b2887932b8de532c4d28

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,5 +1,4 @@
 import unittest
-import mock
 import json
 from flask import Flask
 from ardana_service.admin import bp
@@ -7,12 +6,18 @@ from ardana_service.admin import bp
 app = Flask(__name__)
 app.register_blueprint(bp)
 
+
 class TestAdmin(unittest.TestCase):
 
-    @mock.patch('os.getlogin')
-    def test_get_user(self, mock_os_getlogin):
-        mock_os_getlogin.return_value = "gobbledygook"
+    def test_get_user(self):
+        # Execute without mocking to verify that the real operating system
+        #    calls are being executed without error
         test_app = app.test_client()
         resp = test_app.get('/api/v2/user')
         user_dict = json.loads(resp.data)
-        self.assertEquals(user_dict['username'], 'gobbledygook')
+
+        # Since we cannot control which username that this unit test runs
+        # under, we only require that it return a non-empty username
+        self.assertIn('username', user_dict)
+        username = user_dict['username']
+        self.assertNotEqual('', username)
