539cd9fee51e86ffea632f9715be848215f3914b
Gary Smith <gary.smith@suse.com>
2018-07-31 13:54:30 -0700

    SCRD-3484 Retrieve endpoint info from keystone
    Change-Id: Ieb50380093a83f42ebffd1e63f495b6b48235ef2

--- ardana_service/main.py
+++ ardana_service/main.py
@@ -22,6 +22,7 @@ from ardana_service import admin
 from ardana_service import config  # noqa: F401
 from ardana_service import config_processor
 from ardana_service import encoder
+from ardana_service import keystone
 from ardana_service import listener
 from ardana_service import model
 from ardana_service import playbooks
@@ -65,6 +66,7 @@ app.register_blueprint(admin.bp)
 app.register_blueprint(config_processor.bp)
 app.register_blueprint(playbooks.bp)
 app.register_blueprint(plays.bp)
+app.register_blueprint(keystone.bp)
 app.register_blueprint(listener.bp)
 app.register_blueprint(model.bp)
 app.register_blueprint(servers.bp)
