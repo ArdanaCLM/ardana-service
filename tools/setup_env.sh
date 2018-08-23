a8f7b2bc5bae7d7182088a23bf8b7c2e1ef27640
Gary Smith <gary.smith@suse.com>
2018-08-23 11:17:38 -0700

    SCRD-3485 Add fake playbook, log for running keystone-status
    For development, created a fake keystone-status.yml playbook and
    representative log file.  This facilitates using the new UI menu item
    for running status playbooks.
    
    Change-Id: Id1b7f4cfeaa6f7220ef3a5dbfff7131785f65e58

--- tools/setup_env.sh
+++ tools/setup_env.sh
@@ -55,6 +55,9 @@ if [ ! -d ardana-ansible ] ; then
     git clone ${GIT_BASE}/ardana/ardana-ansible
 fi
 
+# Create a fake playbook to enable keystone-status playbook to run
+touch ardana-ansible/keystone-status.yml
+
 if [ ! -d ardana-input-model ] ; then
     git clone ${GIT_BASE}/ardana/ardana-input-model
 fi
